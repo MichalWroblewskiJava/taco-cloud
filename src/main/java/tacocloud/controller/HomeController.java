4c04e51cb3c0053c65c2728794a7f1963a70c7ac
MichalWroblewskiJava <59879241+MichalWroblewskiJava@users.noreply.github.com>
2020-09-29 11:42:03 +0200

    HomeControllerTestHomeTamplate

---
+++ src/main/java/tacocloud/controller/HomeController.java
@@ -0,0 +1,21 @@
+package tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * klasa obsługje żadania internetowe i udziela na nie odpowiedzi zawierajęce pewne informacje
+ */
+
+/**
+ * @Controller adnotacja jest wykrywana podczas skanowania i tworzy egemplarz HomeController jako komponent bean w kontekście aplikacji Spring.
+ * Przedstawienie opisowe roli danego komponentu w aplikacji
+ */
+@Controller()
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
