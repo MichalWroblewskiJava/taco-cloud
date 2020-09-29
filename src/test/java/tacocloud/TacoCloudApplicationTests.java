92adee1013627b9694a23bf3ec1a5bbe7515d8fe
MichalWroblewskiJava <59879241+MichalWroblewskiJava@users.noreply.github.com>
2020-09-29 10:07:23 +0200

    JUnit annotation added

--- src/test/java/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/tacocloud/TacoCloudApplicationTests.java
@@ -1,11 +1,24 @@
 package tacocloud;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Używany jest menadżer testów SpringRunner
+ */
+@RunWith(SpringRunner.class)
+
+/**
+ * nakazuje JUnit przygotowanie testu z możliwością SpringBoota
+ */
 @SpringBootTest
 class TacoCloudApplicationTests {
 
+    /**
+     * metoda testu
+     */
     @Test
     void contextLoads() {
     }
