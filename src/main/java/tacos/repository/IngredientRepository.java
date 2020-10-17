a51992c5bfb618207a252ce8c64776f1c5abee82
MichalWroblewskiJava <59879241+MichalWroblewskiJava@users.noreply.github.com>
2020-10-17 17:59:29 +0200

    IngredientRepository added, findAll findById save

---
+++ src/main/java/tacos/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package tacos.repository;
+
+import tacos.Ingredient;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Ingredient findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
